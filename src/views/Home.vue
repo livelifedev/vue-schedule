3efde5282f3b6a850400afcfb48b30c74603dc58
John <johnrubio93@gmail.com>
2021-11-16 22:57:52 +1100

    Create main views, fetch data from api

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {},
+  data() {
+    return {
+      data: [],
+    };
+  },
+
+  async created() {
+    const res = await fetch(
+      'https://raw.githubusercontent.com/suyogshiftcare/jsontest/main/available.json'
+    );
+
+    this.data = await res.json();
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped></style>
