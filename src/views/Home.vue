a37c02f12b657dd833ce63c19279390310925c88
John <johnrubio93@gmail.com>
2021-11-16 23:33:23 +1100

    Create coaches list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>{{ data }}</div>
+  <ul>
+    <li v-for="(coach, index) in coaches" :key="index">
+      <router-link to="/schedule">{{ coach }}</router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -10,6 +14,16 @@ export default {
     return {
       data: [],
     };
+  },
+  computed: {
+    coaches() {
+      return this.data.reduce((prev, curr) => {
+        if (!prev.includes(curr.name)) {
+          prev.push(curr.name);
+        }
+        return prev;
+      }, []);
+    },
   },
 
   async created() {
